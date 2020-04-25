8ff1c865341fc117e67200a031d55d617cd961d2
bingfeng yu <ybf0711@gmail.com>
2020-04-25 23:12:38 +0800

    需要优化的列表

--- src/views/music-list.vue
+++ src/views/music-list.vue
@@ -15,34 +15,43 @@
           <span class="text">随机播放全部</span>
         </div>
       </div>
-      <div class="filter" ref="filter"></div>
+      <div class="filter"></div>
     </div>
 
     <ul>
-      <li class="item" v-for="(item, index) in tracks" :key="index">
+      <li
+        class="item"
+        v-for="(item, index) in tracks"
+        :key="index"
+        @click="selectItem()"
+      >
         <div class="content">
-          <h2 class="name">{{item.name}}</h2>
-          <p class="desc"></p>
+          <h2 class="name">{{ item.name }}</h2>
+          <p class="desc">{{ item.ar | merge_songer }}</p>
         </div>
       </li>
     </ul>
-
   </div>
 </template>
 
 <script>
-import { playlist_detail } from '@/api/axios.js'
+import { playlist_detail, song_url } from '@/api/axios.js'
 import loginVue from './login.vue'
 export default {
   data() {
     return {
       playlist_name: 'default',
       tracks: [],
-      coverImgUrl: ''
+      coverImgUrl: '',
+      songer: '',
+      id: '',
+      url: ''
     }
   },
   mounted() {
     playlist_detail(this.$route.query.id).then(res => {
+      console.log(res)
+      this.id = res.data.playlist.trackIds[0].id
       this.playlist_name = res.data.playlist.name
       this.tracks = res.data.playlist.tracks
       this.coverImgUrl = res.data.playlist.coverImgUrl
@@ -51,11 +60,26 @@ export default {
   methods: {
     back() {
       this.$router.back()
+    },
+    async selectItem(song_id) {
+      //console.log(song_id)
+
+      await song_url(this.id).then(res => {
+        console.log(res)
+        this.url = res.data.data[0].url
+        console.log(this.url)
+      })
+      this.$router.push({ path: 'play', query: { song_url: this.url } })
     }
   },
   computed: {
     bgStyle() {
       return `background-image:url(${this.coverImgUrl})`
+    }
+  },
+  filters: {
+    merge_songer(item) {
+      return item.reduce((pre, curr) => pre + curr.name, '')
     }
   }
 }
@@ -89,4 +113,23 @@ export default {
     transform-origin: top
     position: relative
     background-size: cover
+  .item
+    width 100%
+    font-size: $font-size-medium
+    .content
+      padding-left 10%
+      height 4rem
+      line-height 4rem
+      display flex
+      flex-direction column
+      justify-content space-evenly
+      .name
+        height 1rem
+        line-height 1rem
+        justify-content center
+      .desc
+        color: $color-text-d
+        height 1rem
+        line-height 1rem
+        justify-content center
 </style>
