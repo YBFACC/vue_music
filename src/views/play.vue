48117df6449e9d6af869cb02dd3df8283d94b003
bingfeng yu <ybf0711@gmail.com>
2020-04-25 23:12:08 +0800

    简单的能放歌

--- src/views/play.vue
+++ src/views/play.vue
@@ -1,5 +1,25 @@
 <template>
   <div>
     play
+    <audio :src="song_url" ref="audio"  autoplay loop></audio>
+    <button @click="play()">play</button>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      song_url: ''
+    }
+  },
+  mounted() {
+    this.song_url = this.$route.query.song_url
+  },
+  methods: {
+    play(){
+      this.$refs.audio.play()
+    }
+  },
+}
+</script>
