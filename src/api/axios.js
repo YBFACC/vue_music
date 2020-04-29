3f6351b2d283888313b4b696f0bf697e260a6b88
bingfeng yu <ybf0711@gmail.com>
2020-04-29 18:26:09 +0800

    增加歌手页请求

--- src/api/axios.js
+++ src/api/axios.js
@@ -6,8 +6,6 @@ const api = new axios.create({
 
 export const banner = () => api.get('/banner?type=2')
 
-export const recommend = () => api.get('/recommend/resource')
-
 export const login = (phone, password) => {
   let data = {
     phone,
@@ -16,7 +14,20 @@ export const login = (phone, password) => {
   return api.post('/login/cellphone', data)
 }
 
+//获取每日推荐歌单
+export const recommend = () => api.get('/recommend/resource')
+
+//获取歌单详情
 export const playlist_detail = id => api.get('/playlist/detail?id=' + id)
 
+//获取歌曲播放url
+export const song_url = ids => api.get('/song/url?id=' + ids)
 
-export const song_url = song_id => api.get('/song/url?id=' + song_id)
+//获取歌曲详情
+export const song_detail = ids => api.get('/song/detail?ids=' + ids)
+
+//热门歌手
+export const hot_songer = () => api.get('/top/artists?offset=0&limit=10')
+
+//按首字母请求歌手列表
+export const song_list = char => api.get('/artist/list?type=-1&area=-1&limit=5&initial='+char)
