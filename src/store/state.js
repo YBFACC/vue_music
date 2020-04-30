70546ae376906e03ca65ad6c36e5d97959279a8a
bingfeng yu <ybf0711@gmail.com>
2020-04-30 22:08:38 +0800

    songer_list使用localstore储存

--- src/store/state.js
+++ src/store/state.js
@@ -1,9 +1,12 @@
+import { getStore } from '@/utils/store.js'
+
 const state = {
   login: true,
   user_info: {},
   singer: {},
   playing: false,
-  playlist: []
+  playlist: [],
+  songer_list: getStore('songer_list')
 }
 
 export default state
