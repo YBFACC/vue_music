1eb8e1335edfe5a6e9d7f3053bca804b3cab2164
bingfeng yu <ybf0711@gmail.com>
2020-05-03 17:44:45 +0800

    完成样式

---
+++ src/components/search-box/search-box.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="search_box">
+    <i class="icon-search"></i>
+    <input type="text" placeholder="搜索歌曲、歌手" class="search_input" />
+    <i class="icon-dismiss"></i>
+  </div>
+</template>
+
+<style lang="stylus" scoped>
+@import "~@/assets/stylus/variable"
+@import "~@/assets/stylus/mixin"
+
+.search_box
+  height 2.5rem
+  width 100%
+  background: $color-highlight-background
+  border-radius 5px
+  display flex
+  align-items center
+  justify-content space-between
+  box-sizing border-box
+  padding 0 5px
+  .search_input
+    background: $color-highlight-background
+    color: $color-text
+    font-size: $font-size-medium
+    flex 1rem
+    outline none
+    &::placeholder
+      color: $color-text-d
+    &::focus
+      outline none
+  .icon-search
+    font-size: 24px
+    color: $color-background
+</style>
