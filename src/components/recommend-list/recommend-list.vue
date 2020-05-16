2ba34def21d44bd86cd698cc868477944d3f702c
bingfeng yu <ybf0711@gmail.com>
2020-05-16 22:41:26 +0800

    batter-scroll的成功添加

--- src/components/recommend-list/recommend-list.vue
+++ src/components/recommend-list/recommend-list.vue
@@ -1,29 +1,33 @@
 <template>
   <div class="play_list">
-    <h1>每日推荐歌单</h1>
-    <ul>
-      <router-link
-        :to="{ path: '/music_list', query: { id: item.id } }"
-        append
-        tag="li"
-        class="item"
-        v-for="(item, index) in recommend_list"
-        :key="index"
-      >
-        <div class="icon">
-          <img :src="item.picUrl" />
-        </div>
-        <div class="text">
-          <h2 class="name">{{ item.name | brackets }}</h2>
-          <p class="desc">{{ item.creator.nickname }}</p>
-        </div>
-      </router-link>
-    </ul>
+    <Scroll ref="scroll" class="recommend-content" :data="recommend_list">
+      <div>
+        <h1>每日推荐歌单</h1>
+        <ul>
+          <router-link
+            :to="{ path: `/recommend/${item.id}` }"
+            tag="li"
+            class="item"
+            v-for="(item, index) in recommend_list"
+            :key="index"
+          >
+            <div class="icon">
+              <img :src="item.picUrl + '?param=64y64'" />
+            </div>
+            <div class="text">
+              <h2 class="name">{{ item.name }}</h2>
+              <p class="desc">{{ item.creator.nickname }}</p>
+            </div>
+          </router-link>
+        </ul>
+      </div>
+    </Scroll>
   </div>
 </template>
 
 <script>
 import { recommend } from '@/api/axios.js'
+import Scroll from '@/components/scroll/scroll.vue'
 export default {
   data() {
     return {
@@ -32,6 +36,8 @@ export default {
   },
   mounted() {
     recommend().then(res => {
+      console.log(res)
+
       this.recommend_list = res.data.recommend
     })
   },
@@ -41,36 +47,50 @@ export default {
     }
   },
   methods: {
-    // toSongList(id) {
-    //   this.$router.push({ path: '/home/recommend', params: { id: id } })
-    // }
+    select(song_list_id) {}
+  },
+  components: {
+    Scroll
   }
 }
 </script>
 
 <style lang="stylus" scoped>
 .play_list
-  h1
-    text-align center
-    height 4rem
-    line-height 4rem
-  .item
-    display flex
-    align-items center
-    box-sizing border-box
-    padding 0 1.25rem 1.25rem 1.25rem
-    .icon
-      img
-        width 4rem
-        height 4rem
-        margin-right 1.25rem
-    .text
+  position: fixed
+  width: 100%
+  top: 14.5rem
+  bottom: 0
+  .recommend-content
+    height 100%
+    overflow hidden
+    h1
+      text-align center
+      height 4rem
+      line-height 4rem
+    .item
       display flex
-      flex-direction column
-      justify-content center
-      line-height 1.25rem
-      overflow hidden
-      font-size 10px
-      .name
-        margin-bottom: 10px
+      align-items center
+      box-sizing border-box
+      padding 0 1.25rem 1.25rem 1.25rem
+      .icon
+        img
+          width 4rem
+          height 4rem
+          margin-right 1.25rem
+          animation cril 5s  infinite
+      .text
+        display flex
+        flex-direction column
+        justify-content center
+        line-height 1.25rem
+        overflow hidden
+        font-size 10px
+        .name
+          margin-bottom: 10px
+@keyframes cril {
+  100%{
+    transform rotate(360deg)
+  }
+}
 </style>
--- src/views/singer.vue
+++ src/views/singer.vue
@@ -1,28 +1,34 @@
 <template>
   <div class="listview">
-    <ul>
-      <li v-for="(char, index) in songer_list" :key="index">
-        <h2 class="title">{{ char.title }}</h2>
-        <ul>
-          <li
-            v-for="(songer, index) in char.items"
-            :key="index"
-            class="songer_item"
-          >
-            <img :src="songer.picUrl + '?param=64y64'" class="songer_imge" />
-            <span class="songer_name">{{ songer.name }}</span>
-          </li>
-        </ul>
-      </li>
-    </ul>
+    <Scroll ref="scroll" class="warp" :data="songer_list">
+      <ul>
+        <li v-for="(char, index) in songer_list" :key="index">
+          <h2 class="title">{{ char.title }}</h2>
+          <ul>
+            <li
+              v-for="(songer, index) in char.items"
+              :key="index"
+              class="songer_item"
+            >
+              <img :src="songer.picUrl + '?param=64y64'" class="songer_imge" />
+              <span class="songer_name">{{ songer.name }}</span>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </Scroll>
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
+import Scroll from '@/components/scroll/scroll.vue'
 export default {
   computed: {
     ...mapGetters(['songer_list'])
+  },
+  components: {
+    Scroll
   }
 }
 </script>
@@ -30,27 +36,34 @@ export default {
 <style lang="stylus" scoped>
   @import "~@/assets/stylus/variable"
 .listview
-  .title
-    width 100%
-    height: 1.8rem
-    line-height: 1.8rem
-    padding-left: 1.25rem
-    box-sizing border-box
-    font-size: $font-size-small
-    color: $color-text-l
-    background: $color-highlight-background
-  .songer_item
-    width 100%
-    height 4.3rem
-    display flex
-    align-items center
-    box-sizing border-box
-    padding-left 1.8rem
-    .songer_name
+  position fixed
+  top 5.5rem
+  bottom 0
+  width 100%
+  .warp
+    height 100%
+    overflow hidden
+    .title
+      width 100%
+      height: 1.8rem
+      line-height: 1.8rem
+      padding-left: 1.25rem
+      box-sizing border-box
+      font-size: $font-size-small
       color: $color-text-l
-      margin-left 1.2rem
-    .songer_imge
-      width: 3.1rem
-      height 3.1rem
-      border-radius: 50%
+      background: $color-highlight-background
+    .songer_item
+      width 100%
+      height 4.3rem
+      display flex
+      align-items center
+      box-sizing border-box
+      padding-left 1.8rem
+      .songer_name
+        color: $color-text-l
+        margin-left 1.2rem
+      .songer_imge
+        width: 3.1rem
+        height 3.1rem
+        border-radius: 50%
 </style>
