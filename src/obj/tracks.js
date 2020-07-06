588bece33caed4804ecdc5f241dc4e1206f701fa
bingfeng yu <ybf0711@gmail.com>
2020-07-06 22:50:46 +0800

    定义歌词接口

--- src/obj/tracks.js
+++ src/obj/tracks.js
@@ -1,3 +1,5 @@
+import { lyric } from '@/api/axios.js'
+
 export default class filter_tracks {
   constructor({
     song_id,
@@ -14,11 +16,24 @@ export default class filter_tracks {
     this.songer_name = songer_name
     this.album_id = album_id
     this.url = null
+    this.lyric = null
   }
   setUrl(url) {
     this.url = url
   }
-  getLyric() {}
+  getLyric() {
+    return new Promise((resolve, reject) => {
+      lyric(this.song_id).then(res => {
+        if (res.status === 200) {
+          this.lyric = res.data.lrc.lyric
+          console.log(res.data.lrc)
+          resolve(this.lyric)
+        } else {
+          reject(res)
+        }
+      })
+    })
+  }
 }
 
 export function create_filter_tracks(id, name, al, ar) {
