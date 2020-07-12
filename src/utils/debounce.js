eaf1c2afa6d022ef0e7b889deb7961504a08239d
bingfeng yu <ybf0711@gmail.com>
2020-07-12 23:54:04 +0800

    去抖动函数

---
+++ src/utils/debounce.js
@@ -0,0 +1,11 @@
+export default function debounce(fun, delay) {
+  let timer
+  return function(...args) {
+    if (timer) {
+      clearTimeout(timer)
+    }
+    timer = setTimeout(() => {
+      fun.apply(this, args)
+    }, delay)
+  }
+}
